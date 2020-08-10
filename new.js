d3e083d8932b25b12201d0baa150f51861cbe2fa
Max Pahl <42980200+gitcaligo@users.noreply.github.com>
2020-08-10 10:25:23 +0200

    works and is not too ugly code-wise

--- new.js
+++ new.js
@@ -4,10 +4,8 @@ let pointer = 1
 let questionOrder = []
 
 let results = []
-
-let dim1result = 0, dim2result = 0, dim3result = 0, dim4result = 0, dim5result = 0
-let dim1percent = 0, dim2percent = 0, dim3percent = 0, dim4percent = 0, dim5percent = 0
-
+let answerCount = [0,0,0,0,0]
+let answerPercent = [0,0,0,0,0]
 
 // Durstenfeld shuffle
 function shuffleArray(array) {
@@ -35,53 +33,38 @@ function updateDom() {
     $('#progress').css("width", ((pointer - 1) / Object.keys(questions).length * 100).toFixed(0)+"%")
 
     // debug
-    console.log(results)
-}
-
-function calcPercent() {
-
-    for (const prop in results) {
-        if (`${results[prop].answerDimension}` == 'dimension1') {
-            dim1result += parseInt(`${results[prop].answerValue}`)
-        } else if (`${results[prop].answerDimension}` == 'dimension2') {
-            dim2result += parseInt(`${results[prop].answerValue}`)
-        } else if (`${results[prop].answerDimension}` == 'dimension3') {
-            dim3result += parseInt(`${results[prop].answerValue}`)
-        } else if (`${results[prop].answerDimension}` == 'dimension4') {
-            dim4result += parseInt(`${results[prop].answerValue}`)
-        } else if (`${results[prop].answerDimension}` == 'dimension5') {
-            dim5result += parseInt(`${results[prop].answerValue}`)
-        } 
-    }
-
-    for (let i = 1; i <= Object.keys(dimensions).length; i++) {
-        let a = `dim${i}percent`
-        let b = `dim${i}result`
-        `${a}` = (`${b}` / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0)
-        console.log(a)
-        console.log(b)
-    }
-
-    /* dim1percent = (dim1result / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0)
-    dim2percent = (dim2result / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0)
-    dim3percent = (dim3result / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0)
-    dim4percent = (dim4result / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0)
-    dim5percent = (dim5result / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0) */
+    // console.log(results)
 }
 
 function showResult() {
 
-    calcPercent()
+    for (const prop in results) {
+        if (`${results[prop].answerDimension}` == 'dimension1') {
+            answerCount[0] += parseInt(`${results[prop].answerValue}`)
+        } else if (`${results[prop].answerDimension}` == 'dimension2') {
+            answerCount[1] += parseInt(`${results[prop].answerValue}`)
+        } else if (`${results[prop].answerDimension}` == 'dimension3') {
+            answerCount[2] += parseInt(`${results[prop].answerValue}`)
+        } else if (`${results[prop].answerDimension}` == 'dimension4') {
+            answerCount[3] += parseInt(`${results[prop].answerValue}`)
+        } else if (`${results[prop].answerDimension}` == 'dimension5') {
+            answerCount[4] += parseInt(`${results[prop].answerValue}`)
+        } 
+    }
+
+    for (let i = 0; i < Object.keys(dimensions).length; i++) {
+        answerPercent[i] = (answerCount[i] / (Object.keys(questions).length / Object.keys(dimensions).length * 4) * 100).toFixed(0)
+    }
 
     $('#result h2').html('Your results:')
     $('#questions').css('display', 'none')
     $('#result').css('display', 'block') 
 
-    $('#dim1').html(dimensions.dimension1 + ': ' + dim1percent + '%')
-    $('#dim2').html(dimensions.dimension2 + ': ' + dim2percent + '%')
-    $('#dim3').html(dimensions.dimension3 + ': ' + dim3percent + '%')
-    $('#dim4').html(dimensions.dimension4 + ': ' + dim4percent + '%')
-    $('#dim5').html(dimensions.dimension5 + ': ' + dim5percent + '%')
+    $('#dim1').html(dimensions.dimension1 + ': ' + answerPercent[0] + '%')
+    $('#dim2').html(dimensions.dimension2 + ': ' + answerPercent[1] + '%')
+    $('#dim3').html(dimensions.dimension3 + ': ' + answerPercent[2] + '%')
+    $('#dim4').html(dimensions.dimension4 + ': ' + answerPercent[3] + '%')
+    $('#dim5').html(dimensions.dimension5 + ': ' + answerPercent[4] + '%')
 }
 
 $(document).ready( function() {
@@ -118,8 +101,8 @@ $(document).ready( function() {
     $('#reset').click( function() {
         pointer = 1
         results = []
-        dim1result = 0, dim2result = 0, dim3result = 0, dim4result = 0, dim5result = 0
-        dim1percent = 0, dim2percent = 0, dim3percent = 0, dim4percent = 0, dim5percent = 0
+        answerCount = [0,0,0,0,0]
+        answerPercent = [0,0,0,0,0]
         if (config.randomizeQuestions) {
             shuffleArray(questionOrder)
         }
